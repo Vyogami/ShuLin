7fd0c4bf44443022b8fd710338d5a228d403ae77
Priyanshu Verma <priyanv2003@gmail.com>
2023-09-12 14:04:03 +0530

    feat: add actix boilerplate

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(ping))
+        .bind(("127.0.0.1", 5432))?
+        .run()
+        .await
 }
