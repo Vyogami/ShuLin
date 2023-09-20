c8cd1edb9a1e7d0743e4559b2a047c53177d8d94
Priyanshu Verma <priyanv2003@gmail.com>
2023-09-20 21:31:27 +0530

    feat: add basic logging

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,9 @@
 pub mod models;
 pub mod routes;
 
+use actix_web::middleware::Logger;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use env_logger::Env;
 
 #[get("/ping")]
 async fn ping() -> impl Responder {
@@ -10,13 +12,16 @@ async fn ping() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::new("%t \"%r\" %s {{%P}} %T"))
             .service(ping)
             .service(routes::file_permissions::get_perms)
             .service(routes::file_permissions::set_perms)
     })
-    .bind(("127.0.0.1", 5432))?
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await
 }
