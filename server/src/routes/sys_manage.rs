04cddaf691174183a8f72c784d395053cc1264f5
Priyanshu Verma <priyanv2003@gmail.com>
2023-09-21 10:06:30 +0530

    feat: use modprobe blacklist to disable usb

--- server/src/routes/sys_manage.rs
+++ server/src/routes/sys_manage.rs
@@ -1,7 +1,8 @@
 use std::{fs::Permissions, os::unix::prelude::PermissionsExt};
+use tokio::fs;
 
 use crate::models::Toggle;
-use actix_web::{post, web, HttpResponse, Responder};
+use actix_web::{post, web, HttpMessage, HttpResponse, Responder};
 use log::warn;
 use tokio::process::Command;
 
@@ -41,26 +42,35 @@ async fn ssh(ssh_payload: web::Json<Toggle>) -> impl Responder {
 
 #[post("/usb")]
 async fn usb(usb_payload: web::Json<Toggle>) -> impl Responder {
-    let media_dir = match tokio::fs::File::open("/media/").await {
-        Ok(d) => d,
-        Err(e) => {
-            warn!("Could not open /media/: {}", e);
+    if usb_payload.toggle {
+        let mut cmd = Command::new("modprobe");
+        cmd.arg("usb-storage");
+
+        if let Err(e) = cmd.output().await {
+            warn!("Could not run command modprobe usb-storage: {}", e);
             return HttpResponse::InternalServerError().body(e.to_string());
         }
-    };
-    let mode = if usb_payload.toggle {
-        0o777
     } else {
-        0o000
-        // sudo echo "blacklist usb-storage" >> /etc/modprobe.d/blacklist.conf
-    };
+        let mut modprobe_blacklist = match fs::OpenOptions::new()
+            .append(true)
+            .open("/etc/modprobe.d/blacklist.conf")
+            .await
+        {
+            Ok(f) => f,
+            Err(e) => {
+                warn!("Could not open modproble blacklist file: {}", e);
+                return HttpResponse::InternalServerError().body(e.to_string());
+            }
+        };
 
-    if let Err(e) = media_dir
-        .set_permissions(Permissions::from_mode(mode))
-        .await
-    {
-        warn!("Error setting permissions of /media/: {}", e);
-        return HttpResponse::InternalServerError().body(e.to_string());
+        use tokio::io::AsyncWriteExt;
+        if let Err(e) = modprobe_blacklist
+            .write("blacklist usb-storage".as_bytes())
+            .await
+        {
+            warn!("Could not write config to modproble blacklist file: {}", e);
+            return HttpResponse::InternalServerError().body(e.to_string());
+        }
     }
 
     HttpResponse::Ok().finish()
